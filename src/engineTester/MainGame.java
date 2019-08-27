de7f43b2aed654fb0804672a0cd507a85ef01090
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-08-27 20:21:47 +0200

    Added objects loader, and display basic rectangle to window

--- src/engineTester/MainGame.java
+++ src/engineTester/MainGame.java
@@ -4,23 +4,49 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.GL;
 
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 import renderEngine.WindowManager;
 
 public class MainGame {
     public static void main(String[] args) {
         WindowManager.createWindow();
         long window = WindowManager.getWindow();
-        int loop = 0;
+        GL.createCapabilities();
+
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f,
+        };
+
+        RawModel model = loader.loadToVao(vertices);
 
         while (!glfwWindowShouldClose(window)) {
-            //game logic
-            //render
-            GL.createCapabilities();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-            glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
-            WindowManager.updateWindow();
+            //game logic
+
+            //render
+            renderer.prepare();
+            renderer.render(model);
+
+            glfwSwapBuffers(window); // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents();
         }
 
+        loader.cleanUp();
         WindowManager.destroyWindow();
     }
 }
