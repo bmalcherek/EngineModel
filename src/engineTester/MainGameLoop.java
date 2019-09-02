6de2f316632c7d05bb6cbc733d6d95ebdac1937a
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-09-02 12:44:07 +0200

    Loading models from .obj

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -8,115 +8,37 @@ import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
 import entities.Camera;
 import entities.Entity;
 
+import java.io.IOException;
+
 public class MainGameLoop {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
-		
-		float[] vertices = {			
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,0.5f,0,		
-				
-				-0.5f,0.5f,1,	
-				-0.5f,-0.5f,1,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				0.5f,0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				-0.5f,-0.5f,1,	
-				-0.5f,0.5f,1,
-				
-				-0.5f,0.5f,1,
-				-0.5f,0.5f,0,
-				0.5f,0.5f,0,
-				0.5f,0.5f,1,
-				
-				-0.5f,-0.5f,1,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,-0.5f,1
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
 
-				
-		};
+		RawModel pistonModel = OBJLoader.loadObjModel("piston", loader);
+		TexturedModel pistonStaticModel = new TexturedModel(pistonModel,new ModelTexture(loader.loadTexture("steel")));
 		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
-		
-		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("image")));
-		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		Entity pistonEntity = new Entity(pistonStaticModel, new Vector3f(0,0,-80),0,0,0,1);
 		
 		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()){
-			entity.increaseRotation(1, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
 			shader.loadViewMatrix(camera);
-			renderer.render(entity,shader);
+			renderer.render(pistonEntity,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
